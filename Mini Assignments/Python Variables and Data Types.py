917e5abcb1a30ed3a8c04126e11c6ba64d5eef5b
diegipoo <diego.ortega@scholar.smash.org>
2022-04-13 10:11:55 -0700

    Done

---
+++ Mini Assignments/Python Variables and Data Types.py
@@ -0,0 +1,19 @@
+print ("Hello World")    #Hello World string
+
+name = "Kiwi"
+age = 17
+years_at_HtH = 0
+plays_basketball = False
+jersey_number = "N/A"
+car_name = "Honda", "Acura", "Toyota"
+x = 50
+first_name = "Diego"
+
+print (name)
+print (age)
+print (years_at_HtH)
+print (plays_basketball)
+print (jersey_number)
+print (car_name)
+print (x)
+print (first_name)
